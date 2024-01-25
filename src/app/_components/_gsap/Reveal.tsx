982500b2126e59997a5783d7657adf9b942aba37
lisandrojm <lisandrojm@gmail.com>
2024-01-25 17:18:23 -0300

    refactoring

--- src/app/_components/_gsap/Reveal.tsx
+++ src/app/_components/_gsap/Reveal.tsx
@@ -1,5 +1,8 @@
+// src/app/_components/_gsap/GsapScrollReveal.tsx
+
 import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 interface RevealProps {
@@ -10,73 +13,76 @@ interface RevealProps {
 const Reveal: React.FC<RevealProps> = ({ children, className }) => {
   const container = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const elem = container.current;
+  useGSAP(
+    (context, contextSafe) => {
+      const elem = container.current;
 
-    if (!elem) return;
+      if (!elem) return;
 
-    const animateFrom = (direction: number) => {
-      direction = direction || 1;
-      let x = 0,
-        y = direction * 100;
-      let duration = 1.5;
+      const animateFrom = (direction: number) => {
+        direction = direction || 1;
+        let x = 0,
+          y = direction * 100;
+        let duration = 1.5;
 
-      if (elem.classList.contains('gs_reveal_fromLeft')) {
-        x = -100;
-        y = 0;
-        duration = 1.5;
-      } else if (elem.classList.contains('gs_reveal_fromRight')) {
-        x = 100;
-        y = 0;
-        duration = 1.5;
-      } else if (elem.classList.contains('gs_reveal_fromBottom')) {
-        x = 0;
-        y = 100;
-        duration = 1.5;
-      } else if (elem.classList.contains('gs_reveal_fromTop')) {
-        x = 0;
-        y = -100;
-        duration = 1.5;
-      }
-
-      elem.style.transform = `translate(${x}px, ${y}px)`;
-      elem.style.opacity = '0';
-
-      gsap.fromTo(
-        elem,
-        { x: x, y: y, autoAlpha: 0 },
-        {
-          duration: duration,
-          x: 0,
-          y: 0,
-          autoAlpha: 1,
-          ease: 'expo',
-          overwrite: 'auto',
+        if (elem.classList.contains('gs_reveal_fromLeft')) {
+          x = -100;
+          y = 0;
+          duration = 1.5;
+        } else if (elem.classList.contains('gs_reveal_fromRight')) {
+          x = 100;
+          y = 0;
+          duration = 1.5;
+        } else if (elem.classList.contains('gs_reveal_fromBottom')) {
+          x = 0;
+          y = 100;
+          duration = 1.5;
+        } else if (elem.classList.contains('gs_reveal_fromTop')) {
+          x = 0;
+          y = -100;
+          duration = 1.5;
         }
-      );
-    };
 
-    const onEnter = () => {
-      animateFrom(1);
-    };
+        elem.style.transform = `translate(${x}px, ${y}px)`;
+        elem.style.opacity = '0';
 
-    const onEnterBack = () => {
-      animateFrom(-1);
-    };
+        gsap.fromTo(
+          elem,
+          { x: x, y: y, autoAlpha: 0 },
+          {
+            duration: duration,
+            x: 0,
+            y: 0,
+            autoAlpha: 1,
+            ease: 'expo',
+            overwrite: 'auto',
+          }
+        );
+      };
 
-    const onLeave = () => {
-      // Removed hide() as it was not being used
-    };
+      const onEnter = () => {
+        animateFrom(1);
+      };
 
-    gsap.registerPlugin(ScrollTrigger);
+      const onEnterBack = () => {
+        animateFrom(-1);
+      };
 
-    ScrollTrigger.create({
-      trigger: elem,
-      onEnter,
-      onEnterBack,
-      onLeave,
-    });
-  }, []); // Empty dependency array ensures that the effect runs only once after mount
+      const onLeave = () => {
+        // Removed hide() as it was not being used
+      };
+
+      gsap.registerPlugin(ScrollTrigger);
+
+      ScrollTrigger.create({
+        trigger: elem,
+        onEnter,
+        onEnterBack,
+        onLeave,
+      });
+    },
+    { scope: container }
+  );
 
   return (
     <div ref={container} className={className}>
