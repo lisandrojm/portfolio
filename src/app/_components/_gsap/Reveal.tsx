2d7bd10ab1782cee147725a297239e1a45111104
lisandrojm <lisandrojm@gmail.com>
2024-01-25 07:31:06 -0300

    vercel-testing

--- src/app/_components/_gsap/Reveal.tsx
+++ src/app/_components/_gsap/Reveal.tsx
@@ -2,9 +2,8 @@
 
 import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-
-gsap.registerPlugin(ScrollTrigger);
 
 interface RevealProps {
   children: React.ReactNode;
@@ -12,83 +11,85 @@ interface RevealProps {
 }
 
 const Reveal: React.FC<RevealProps> = ({ children, className }) => {
-  const element = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const elem = element.current;
-    if (!elem) return;
+  useGSAP(
+    (context, contextSafe) => {
+      const elem = container.current;
 
-    const animateFrom = (direction: number) => {
-      direction = direction || 1;
-      let x = 0,
-        y = direction * 100;
-      let duration = 1.25;
+      if (!elem) return;
 
-      if (elem.classList.contains('gs_reveal_fromLeft')) {
-        x = -100;
-        y = 0;
-        duration = 1.25;
-      } else if (elem.classList.contains('gs_reveal_fromRight')) {
-        x = 100;
-        y = 0;
-        duration = 1.25;
-      } else if (elem.classList.contains('gs_reveal_fromBottomF')) {
-        x = 0;
-        y = 100;
-        duration = 1.25;
-      } else if (elem.classList.contains('gs_reveal_fromBottomS')) {
-        x = 0;
-        y = 100;
-        duration = 1.25;
-      } else if (elem.classList.contains('gs_reveal_fromUp')) {
-        x = 0;
-        y = -100;
-        duration = 1.25;
-      }
+      const animateFrom = (direction: number) => {
+        direction = direction || 1;
+        let x = 0,
+          y = direction * 100;
+        let duration = 1.25;
 
-      elem.style.transform = `translate(${x}px, ${y}px)`;
-      elem.style.opacity = '0';
-
-      gsap.fromTo(
-        elem,
-        { x: x, y: y, autoAlpha: 0 },
-        {
-          duration: duration,
-          x: 0,
-          y: 0,
-          autoAlpha: 1,
-          ease: 'expo',
-          overwrite: 'auto',
+        if (elem.classList.contains('gs_reveal_fromLeft')) {
+          x = -100;
+          y = 0;
+          duration = 1.25;
+        } else if (elem.classList.contains('gs_reveal_fromRight')) {
+          x = 100;
+          y = 0;
+          duration = 1.25;
+        } else if (elem.classList.contains('gs_reveal_fromBottomF')) {
+          x = 0;
+          y = 100;
+          duration = 1.25;
+        } else if (elem.classList.contains('gs_reveal_fromBottomS')) {
+          x = 0;
+          y = 100;
+          duration = 1.25;
+        } else if (elem.classList.contains('gs_reveal_fromUp')) {
+          x = 0;
+          y = -100;
+          duration = 1.25;
         }
-      );
-    };
 
-    const hide = () => {};
+        elem.style.transform = `translate(${x}px, ${y}px)`;
+        elem.style.opacity = '0';
 
-    const onEnter = () => {
-      animateFrom(1);
-    };
+        gsap.fromTo(
+          elem,
+          { x: x, y: y, autoAlpha: 0 },
+          {
+            duration: duration,
+            x: 0,
+            y: 0,
+            autoAlpha: 1,
+            ease: 'expo',
+            overwrite: 'auto',
+          }
+        );
+      };
 
-    const onEnterBack = () => {
-      animateFrom(-1);
-    };
+      const onEnter = () => {
+        animateFrom(1);
+      };
 
-    const onLeave = () => {
-      hide();
-    };
+      const onEnterBack = () => {
+        animateFrom(-1);
+      };
 
-    hide();
+      const onLeave = () => {
+        // Removed hide() as it was not being used
+      };
 
-    ScrollTrigger.create({
-      trigger: elem,
-      onEnter,
-      onEnterBack,
-      onLeave,
-    });
-  }, []);
+      gsap.registerPlugin(ScrollTrigger);
+
+      ScrollTrigger.create({
+        trigger: elem,
+        onEnter,
+        onEnterBack,
+        onLeave,
+      });
+    },
+    { scope: container }
+  );
 
   return (
-    <div ref={element} className={className}>
+    <div ref={container} className={className}>
       {children}
     </div>
   );
