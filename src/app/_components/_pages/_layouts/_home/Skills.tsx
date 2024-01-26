c6a8b49821f825cdc4bc4c24f148cc8d75537f81
lisandrojm <lisandrojm@gmail.com>
2024-01-26 08:22:52 -0300

    scale-icons

--- src/app/_components/_pages/_layouts/_home/Skills.tsx
+++ src/app/_components/_pages/_layouts/_home/Skills.tsx
@@ -9,6 +9,7 @@ import XContainer from '@/_components/_containers/XContainer';
 import YMContainer from '@/_components/_containers/YMContainer';
 import YPContainer from '@/_components/_containers/YPContainer';
 import TranslateInOut from '@/_components/_gsap/TranslateInOut';
+import ScaleInOut from '@/_components/_gsap/ScaleInOut';
 
 export default function Skills() {
   return (
@@ -35,65 +36,132 @@ export default function Skills() {
             <div className="flex justify-start md:justify-center">
               <div className="flex flex-col gap-12 text-2xl font-bold uppercase md:w-1/2 md:flex-row md:flex-wrap md:gap-20">
                 <div>
-                  <div className="mb-2">
-                    <h2>Frontend</h2>
-                  </div>
+                  <TranslateInOut overflowHidden delay={0.2} y={100} start="-100% bottom" end="top top" watch>
+                    <div className="mb-2">
+                      <h2>Frontend</h2>
+                    </div>
+                  </TranslateInOut>
                   <div className="flex flex-wrap gap-3 text-3xl md:text-4xl">
-                    <LinkIcon kind="html" ariaLabel="Html Icon" />
-                    <LinkIcon kind="css" ariaLabel="Css Icon" />
-                    <LinkIcon kind="js" ariaLabel="JavaScript Icon" />
-                    <LinkIcon kind="ts" ariaLabel="TypeScript Icon" />
-                    <LinkIcon kind="react" ariaLabel="Reactjs Icon" />
-                    <LinkIcon kind="nextjs" ariaLabel="Nextjs Icon" />
-                    <LinkIcon kind="bootstrap" ariaLabel="Bootstrap Icon" />
-                    <LinkIcon kind="tailwind" ariaLabel="Tailwindcss Icon" />
-                    <LinkIcon kind="docker" ariaLabel="Docker Icon" />
+                    <ScaleInOut delay={0.1} ease="elastic.out" watch>
+                      <LinkIcon kind="html" ariaLabel="Html Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.2} ease="elastic.out" watch>
+                      <LinkIcon kind="css" ariaLabel="Css Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.3} ease="elastic.out" watch>
+                      <LinkIcon kind="js" ariaLabel="JavaScript Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.4} ease="elastic.out" watch>
+                      <LinkIcon kind="ts" ariaLabel="TypeScript Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.5} ease="elastic.out" watch>
+                      <LinkIcon kind="ts" ariaLabel="TypeScript Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.6} ease="elastic.out" watch>
+                      <LinkIcon kind="react" ariaLabel="Reactjs Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.7} ease="elastic.out" watch>
+                      <LinkIcon kind="nextjs" ariaLabel="Nextjs Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.8} ease="elastic.out" watch>
+                      <LinkIcon kind="bootstrap" ariaLabel="Bootstrap Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.9} ease="elastic.out" watch>
+                      <LinkIcon kind="tailwind" ariaLabel="Tailwindcss Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={1} ease="elastic.out" watch>
+                      <LinkIcon kind="docker" ariaLabel="Docker Icon" />
+                    </ScaleInOut>
                   </div>
                 </div>
                 <div>
-                  <div className="mb-2">
-                    <h2>Backend</h2>
-                  </div>
+                  <TranslateInOut overflowHidden delay={0.2} y={100} start="-100% bottom" end="top top" watch>
+                    <div className="mb-2">
+                      <h2>Backend</h2>
+                    </div>
+                  </TranslateInOut>
                   <div className="flex flex-wrap gap-3 text-3xl text-orange md:text-4xl">
-                    <LinkIcon kind="nodejs" ariaLabel="Nodejs Icon" />
-                    <LinkIcon kind="express" ariaLabel="Express Icon" />
-                    <LinkIcon kind="nestjs" ariaLabel="Nestjs Icon" />
-                    <LinkIcon kind="mongodb" ariaLabel="Mongodb Icon" />
+                    <ScaleInOut delay={0.1} ease="elastic.out" watch>
+                      <LinkIcon kind="nodejs" ariaLabel="Nodejs Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.2} ease="elastic.out" watch>
+                      <LinkIcon kind="express" ariaLabel="Express Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.3} ease="elastic.out" watch>
+                      <LinkIcon kind="nestjs" ariaLabel="Nestjs Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.4} ease="elastic.out" watch>
+                      <LinkIcon kind="mongodb" ariaLabel="Mongodb Icon" />
+                    </ScaleInOut>
                   </div>
                 </div>
                 <div>
-                  <div className="mb-2">
-                    <h2>Software & tools</h2>
-                  </div>
+                  <TranslateInOut overflowHidden delay={0.2} y={100} start="-100% bottom" end="top top" watch>
+                    <div className="mb-2">
+                      <h2>Software & tools</h2>
+                    </div>
+                  </TranslateInOut>
                   <div className="flex flex-wrap gap-3 text-3xl text-orange md:text-4xl">
-                    <LinkIcon kind="apple" ariaLabel="Apple Icon" />
-                    <LinkIcon kind="linux" ariaLabel="Linux Icon" />
-                    <LinkIcon kind="windows" ariaLabel="Windows Icon" />
-                    <LinkIcon kind="vscode" ariaLabel="Visual Studio Code Icon" />
-                    <LinkIcon kind="neovim" ariaLabel="Neovim Icon" />
-                    <LinkIcon kind="git" ariaLabel="Git Icon" />
-                    <LinkIcon kind="github" ariaLabel="GitHub Icon" />
+                    <ScaleInOut delay={0.1} ease="elastic.out" watch>
+                      <LinkIcon kind="apple" ariaLabel="Apple Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.2} ease="elastic.out" watch>
+                      <LinkIcon kind="linux" ariaLabel="Linux Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.3} ease="elastic.out" watch>
+                      <LinkIcon kind="windows" ariaLabel="Windows Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.4} ease="elastic.out" watch>
+                      <LinkIcon kind="vscode" ariaLabel="Visual Studio Code Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.5} ease="elastic.out" watch>
+                      <LinkIcon kind="neovim" ariaLabel="Neovim Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.6} ease="elastic.out" watch>
+                      <LinkIcon kind="git" ariaLabel="Git Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.7} ease="elastic.out" watch>
+                      <LinkIcon kind="github" ariaLabel="GitHub Icon" />
+                    </ScaleInOut>
                   </div>
                 </div>
                 <div>
-                  <div className="mb-2">
-                    <h2>Cloud hosting</h2>
-                  </div>
+                  <TranslateInOut overflowHidden delay={0.2} y={100} start="-100% bottom" end="top top" watch>
+                    <div className="mb-2">
+                      <h2>Cloud hosting</h2>
+                    </div>
+                  </TranslateInOut>
                   <div className="flex flex-wrap gap-3 text-3xl text-orange md:text-4xl">
-                    <LinkIcon kind="vercel" ariaLabel="Vercel Icon" />
-                    <LinkIcon kind="railway" ariaLabel="Railway Icon" />
+                    <ScaleInOut delay={0.1} ease="elastic.out" watch>
+                      <LinkIcon kind="vercel" ariaLabel="Vercel Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.2} ease="elastic.out" watch>
+                      <LinkIcon kind="railway" ariaLabel="Railway Icon" />
+                    </ScaleInOut>
                   </div>
                 </div>
                 <div>
-                  <div className="mb-2">
-                    <h2>Design</h2>
-                  </div>
+                  <TranslateInOut overflowHidden delay={0.2} y={100} start="-100% bottom" end="top top" watch>
+                    <div className="mb-2">
+                      <h2>Design</h2>
+                    </div>
+                  </TranslateInOut>
                   <div className="flex flex-wrap gap-3 text-3xl text-orange md:text-4xl">
-                    <LinkIcon kind="figma" ariaLabel="Figma Icon" />
-                    <LinkIcon kind="illustrator" ariaLabel="Iullustrator Icon" />
-                    <LinkIcon kind="photoshop" ariaLabel="Photoshop Icon" />
-                    <LinkIcon kind="aftereffects" ariaLabel="After Effects Icon" />
-                    <LinkIcon kind="premiere" ariaLabel="Premier Pro Icon" />
+                    <ScaleInOut delay={0.1} ease="elastic.out" watch>
+                      <LinkIcon kind="figma" ariaLabel="Figma Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.2} ease="elastic.out" watch>
+                      <LinkIcon kind="illustrator" ariaLabel="Iullustrator Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.3} ease="elastic.out" watch>
+                      <LinkIcon kind="photoshop" ariaLabel="Photoshop Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.3} ease="elastic.out" watch>
+                      <LinkIcon kind="aftereffects" ariaLabel="After Effects Icon" />
+                    </ScaleInOut>
+                    <ScaleInOut delay={0.4} ease="elastic.out" watch>
+                      <LinkIcon kind="premiere" ariaLabel="Premier Pro Icon" />
+                    </ScaleInOut>
                   </div>
                 </div>
               </div>
