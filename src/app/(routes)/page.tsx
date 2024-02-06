6eee2a0f66b8f477ac8b4432d1972ef862837a3d
lisandrojm <lisandrojm@gmail.com>
2024-02-06 05:54:39 -0300

    loading-fix

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,6 +1,5 @@
-/* src/app/(routes)/page.tsx */
-
-/* import LayoutWrapper from '@/_components/_containers/LayoutWrapper';
+// src/app/(routes)/page.tsx
+import LayoutWrapper from '@/_components/_containers/LayoutWrapper';
 import Hero from '@/_components/_pages/_layouts/_home/Hero';
 import Works from '@/_components/_pages/_layouts/_home/Works';
 import Skills from '@/_components/_pages/_layouts/_home/Skills';
@@ -18,37 +17,3 @@ export default function RootHome() {
     </LayoutWrapper>
   );
 }
- */
-/* src/app/(routes)/page.tsx */
-
-'use client';
-import React, { useState } from 'react';
-import LayoutWrapper from '@/_components/_containers/LayoutWrapper';
-import Hero from '@/_components/_pages/_layouts/_home/Hero';
-import Works from '@/_components/_pages/_layouts/_home/Works';
-import Skills from '@/_components/_pages/_layouts/_home/Skills';
-import About from '@/_components/_pages/_layouts/_home/About';
-import Contact from '@/_components/_pages/_layouts/_home/Contact';
-import RootLoading from '@/(routes)/loading';
-
-const RootHome = () => {
-  const [loading, setLoading] = useState(true);
-
-  const handleLoadingComplete = () => {
-    setLoading(false);
-  };
-
-  return loading ? (
-    <RootLoading onLoadingComplete={handleLoadingComplete} />
-  ) : (
-    <LayoutWrapper>
-      <Hero />
-      <Works />
-      <Skills />
-      <About />
-      <Contact />
-    </LayoutWrapper>
-  );
-};
-
-export default RootHome;
