1c541bcecfccc97a7def111508ef4151732641bc
lisandrojm <lisandrojm@gmail.com>
2024-02-09 06:50:14 -0300

    loading-time

--- src/app/(routes)/loading.tsx
+++ src/app/(routes)/loading.tsx
@@ -51,7 +51,7 @@ const RootLoading: React.FC<RootLoadingProps> = ({ onLoadingComplete }) => {
           <div className="flex h-svh flex-col items-center justify-center">
             <h1 className="flex items-center">
               <FadeOut durationOut={1} delayOut={0.1} onComplete={onLoadingComplete}>
-                <span className="gs_reveal_fromBottom text-md font-mono text-xl text-orange">lisandrojm</span>
+                <span className="gs_reveal_fromBottom text-md font-mono text-xl text-orange">lisandrojmz</span>
                 <span className="gs_reveal_fromBottom text-md px-2 font-mono text-xl text-white">|</span>
                 <span className="font-serif text-2xl">Portfolio</span>
               </FadeOut>
