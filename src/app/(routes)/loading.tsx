a3afe7a6eda774bb519889580aadfcf1eefba691
lisandrojm <lisandrojm@gmail.com>
2024-02-09 07:41:02 -0300

    loading-timeout

--- src/app/(routes)/loading.tsx
+++ src/app/(routes)/loading.tsx
@@ -33,23 +33,16 @@ const RootLoading: React.FC<RootLoadingProps> = ({ onLoadingComplete }) => {
 
       const loadTime = loadEnd - loadStart;
 
+      const minimumLoadTime = 4000; // 4 segundos
+
       const timeout = setTimeout(
         () => {
           onLoadingComplete();
         },
-        Math.min(1000, loadTime) // Espera mínimo 1 segundo
+        Math.max(minimumLoadTime, loadTime) // Espera el máximo entre el tiempo mínimo y el tiempo real de carga
       );
 
-      // Lógica para agregar un setTimeout de 4 segundos después de la carga
-      const additionalTimeout = setTimeout(() => {
-        onLoadingComplete();
-      }, 9000);
-
-      // Limpiar los timeouts al desmontar el componente
-      return () => {
-        clearTimeout(timeout);
-        clearTimeout(additionalTimeout);
-      };
+      return () => clearTimeout(timeout);
     }
   }, [onLoadingComplete]);
 
@@ -60,7 +53,7 @@ const RootLoading: React.FC<RootLoadingProps> = ({ onLoadingComplete }) => {
           <div className="flex h-svh flex-col items-center justify-center">
             <h1 className="flex items-center">
               <FadeOut durationOut={1} delayOut={0.1} onComplete={onLoadingComplete}>
-                <span className="gs_reveal_fromBottom text-md font-mono text-xl text-orange">lisandrojmx</span>
+                <span className="gs_reveal_fromBottom text-md font-mono text-xl text-orange">lisandrojm4</span>
                 <span className="gs_reveal_fromBottom text-md px-2 font-mono text-xl text-white">|</span>
                 <span className="font-serif text-2xl">Portfolio</span>
               </FadeOut>
